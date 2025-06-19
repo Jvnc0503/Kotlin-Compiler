6f6377fb61938a84d846840b19687b2bf24c38eb
m41k1 <michalhinojosa@gmail.com>
2025-06-18 23:49:19 -0500

    feat: exp.cpp y exp.

--- src/exp.cpp
+++ src/exp.cpp
@@ -1,94 +1,151 @@
-#include <iostream>
 #include "exp.h"
+
+#include <iostream>
+
 #include "visitor.h"
 
 using namespace std;
 
-BinaryExp::BinaryExp(Exp* l, Exp* r, BinaryOp op):left(l),right(r),op(op) {
+BinaryExp::BinaryExp(Exp* l, Exp* r, BinaryOp op) : left(l), right(r), op(op) {
     if (op == PLUS_OP || op == MINUS_OP || op == MUL_OP || op == DIV_OP) {
         type = "int";
     } else {
         type = "bool";
     }
 }
-NumberExp::NumberExp(int v):value(v) {}
-BoolExp::BoolExp(bool v):value(v) {}
-IdentifierExp::IdentifierExp(const string& n):name(n) {}
+NumberExp::NumberExp(int v) : value(v) {}
+BoolExp::BoolExp(bool v) : value(v) {}
+IdentifierExp::IdentifierExp(const string& n) : name(n) {}
 Exp::~Exp() {}
-BinaryExp::~BinaryExp() { delete left; delete right; }
+BinaryExp::~BinaryExp() {
+    delete left;
+    delete right;
+}
 
-NumberExp::~NumberExp() { }
-BoolExp::~BoolExp() { }
-IdentifierExp::~IdentifierExp() { }
-AssignStatement::AssignStatement(string id, Exp* e): id(id), rhs(e) {}
+NumberExp::~NumberExp() {}
+BoolExp::~BoolExp() {}
+IdentifierExp::~IdentifierExp() {}
+UnaryExp::UnaryExp(Exp* e, UnaryOp o) : exp(e), op(o){};
+UnaryExp::~UnaryExp() {
+    delete exp;
+}
+AssignStatement::AssignStatement(string id, Exp* e) : id(id), rhs(e) {}
 AssignStatement::~AssignStatement() {
     delete rhs;
 }
-PrintStatement::PrintStatement(Exp* e): e(e) {}
+PrintStatement::PrintStatement(Exp* e, string t) : exp(e), type(t) {}
 PrintStatement::~PrintStatement() {
-    delete e;
+    delete exp;
 }
 
-IfStatement::IfStatement(Exp* c, Body* t, Body* e): condition(c), then(t), els(e) {}
+IfStatement::IfStatement(Exp* c, Block* t, Block* e) : condition(c), then(t), els(e) {}
 IfStatement::~IfStatement() {
     delete condition;
     delete then;
     delete els;
 }
 
-ForStatement::ForStatement(Exp* c, Stm* a, Stm* i, Body* b): compare(c), assign(a), increment(i), body(b) {}
 ForStatement::~ForStatement() {
-  delete compare;
-  delete assign;
-  delete increment;
-  delete body;
+    delete begin;
+    delete end;
+    delete block;
 }
 
-VarDec::VarDec(string type, list<string> ids): type(type), vars(ids) {}
-VarDec::~VarDec() {}
-
-VarDecList::VarDecList(): vardecs() {}
-void VarDecList::add(VarDec* v) {
-    vardecs.push_back(v);
+ReturnStatement::ReturnStatement(Exp* r) : ret(r) {}
+ReturnStatement::~ReturnStatement() {
+    delete ret;
 }
+
+WhileStatement::WhileStatement(Exp* c, Block* b) : condition(c), block(b) {}
+WhileStatement::~WhileStatement() {
+    delete condition;
+    delete block;
+}
+
+VarDec::~VarDec() {
+    delete exp;
+}
+
 VarDecList::~VarDecList() {
-    for (auto v: vardecs) {
+    for (auto v : vardecs) {
         delete v;
     }
 }
-
-StatementList::StatementList(): stms() {}
-void StatementList::add(Stm* s) {
-    stms.push_back(s);
+void VarDecList::add(VarDec* v) {
+    vardecs.push_back(v);
 }
-StatementList::~StatementList() {
-    for (auto s: stms) {
+
+StmtList::~StmtList() {
+    for (auto s : stm_list) {
         delete s;
     }
 }
 
-Body::Body(VarDecList* v, StatementList* s): vardecs(v), slist(s) {}
-Body::~Body() {
-    delete vardecs;
-    delete slist;
+Block::~Block() {
+    delete vardecl;
+    delete stmdecl;
 }
-
-
-Program::Program(Body* b): body(b) {}
 
 Program::~Program() {
-    delete body;
+    if (vl)
+        delete vl;
+    if (fl)
+        delete fl;
+    if (cl)
+        delete cl;
+    if (stml)
+        delete stml;
 }
 Stm::~Stm() {}
-char Exp::binopToChar(BinaryOp op) {
-    char  c=' ';
-    switch(op) {
-        case PLUS_OP: c = '+'; break;
-        case MINUS_OP: c = '-'; break;
-        case MUL_OP: c = '*'; break;
-        case DIV_OP: c = '/'; break;
-        case LT_OP: c = '<'; break;
-        default: c = '$';
+string Exp::binopToString(BinaryOp op) {
+    switch (op) {
+        case PLUS_OP:
+            return "+";
+        case MINUS_OP:
+            return "-";
+        case MUL_OP:
+            return "*";
+        case DIV_OP:
+            return "/";
+        case LT_OP:
+            return "<";
+        case LE_OP:
+            return "<=";
+        case GT_OP:
+            return ">";
+        case GE_OP:
+            return ">=";
+        case NE_OP:
+            return "!=";
+        case EQ_OP:
+            return "==";
+        case OR_OP:
+            return "||";
+        case AND_OP:
+            return "&&";
+        default:
+            return "$";
     }
-    return c;
+}
+
+ClassDec::~ClassDec() {
+    for (auto v : vardecs) {
+        delete v;
+    }
+}
+
+ClassDecList::~ClassDecList() {
+    for (auto c : classdecs) {
+        delete c;
+    }
+}
+
+FunDec::~FunDec() {
+    delete block;
+}
+
+FunDecList::~FunDecList() {
+    for (auto f : fundecs) {
+        delete f;
+    }
 }
--- src/exp.h
+++ src/exp.h
@@ -8,143 +8,229 @@
 class Visitor;
 
 using namespace std;
-enum BinaryOp { PLUS_OP, MINUS_OP, MUL_OP, DIV_OP, LT_OP, LE_OP, EQ_OP };
+enum BinaryOp {
+    PLUS_OP,
+    MINUS_OP,
+    MUL_OP,
+    DIV_OP,
+    LT_OP,
+    LE_OP,
+    GT_OP,
+    GE_OP,
+    NE_OP,
+    EQ_OP,
+    OR_OP,
+    AND_OP,
+};
+enum UnaryOp { NOT_OP, NEG_OP };
 
-class Body;
+class FunDec;
+class VarDec;
+class Block;
+class Programm;
+class StmDec;
+class VarDecList;
+class StmtList;
 
 class Exp {
- public:
-  virtual int accept(Visitor* visitor) = 0;
-  virtual ~Exp() = 0;
-  static char binopToChar(BinaryOp op);
+   public:
+    virtual int accept(Visitor* visitor) = 0;
+    virtual ~Exp() = 0;
+    static string binopToString(BinaryOp op);
 };
 
 class BinaryExp : public Exp {
- public:
-  Exp *left, *right;
-  string type;
-  BinaryOp op;
-  BinaryExp(Exp* l, Exp* r, BinaryOp op);
-  int accept(Visitor* visitor);
-  ~BinaryExp();
+   public:
+    Exp *left, *right;
+    string type;
+    BinaryOp op;
+    BinaryExp(Exp* l, Exp* r, BinaryOp op);
+    int accept(Visitor* visitor);
+    ~BinaryExp();
+};
+
+class UnaryExp : public Exp {
+   public:
+    Exp* exp;
+    UnaryOp op;
+    UnaryExp(Exp* e, UnaryOp op);
+    ~UnaryExp();
+    int accept(Visitor* visitor);
 };
 
 class NumberExp : public Exp {
- public:
-  int value;
-  NumberExp(int v);
-  int accept(Visitor* visitor);
-  ~NumberExp();
+   public:
+    int value;
+    NumberExp(int v);
+    int accept(Visitor* visitor);
+    ~NumberExp();
 };
 
 class BoolExp : public Exp {
- public:
-  int value;
-  BoolExp(bool v);
-  int accept(Visitor* visitor);
-  ~BoolExp();
+   public:
+    int value;
+    BoolExp(bool v);
+    int accept(Visitor* visitor);
+    ~BoolExp();
 };
 
 class IdentifierExp : public Exp {
- public:
-  std::string name;
-  IdentifierExp(const std::string& n);
-  int accept(Visitor* visitor);
-  ~IdentifierExp();
+   public:
+    std::string name;
+    IdentifierExp(const std::string& n);
+    int accept(Visitor* visitor);
+    ~IdentifierExp();
 };
 
 class Stm {
- public:
-  virtual int accept(Visitor* visitor) = 0;
-  virtual ~Stm() = 0;
+   public:
+    virtual int accept(Visitor* visitor) = 0;
+    virtual ~Stm() = 0;
+};
+
+class StmtList {
+   public:
+    list<Stm*> stm_list;
+    StmtList(){};
+    ~StmtList();
+    void add(Stm* s) { stm_list.push_back(s); }
+    int accept(Visitor* visitor);
 };
 
 class AssignStatement : public Stm {
- public:
-  std::string id;
-  Exp* rhs;
-  AssignStatement(std::string id, Exp* e);
-  int accept(Visitor* visitor);
-  ~AssignStatement();
+   public:
+    std::string id;
+    Exp* rhs;
+    AssignStatement(std::string id, Exp* e);
+    AssignStatement(){};
+    int accept(Visitor* visitor);
+    ~AssignStatement();
 };
 
 class PrintStatement : public Stm {
- public:
-  Exp* e;
-  PrintStatement(Exp* e);
-  int accept(Visitor* visitor);
-  ~PrintStatement();
+   public:
+    string type;
+    Exp* exp;
+    PrintStatement(Exp* e, string t);
+    PrintStatement(){};
+    int accept(Visitor* visitor);
+    ~PrintStatement();
+};
+
+class ReturnStatement : public Stm {
+   public:
+    Exp* ret;
+    ReturnStatement(Exp* r);
+    ~ReturnStatement();
+    int accept(Visitor* visitor);
 };
 
 class ForStatement : public Stm {
- public:
-  Exp* compare;
-  Stm* assign;
-  Stm* increment;
-  Body* body;
-  ForStatement(Exp* c, Stm* a, Stm* i, Body* b);
-  int accept(Visitor* visitor);
-  ~ForStatement();
+   public:
+    string id;
+    Exp* begin;
+    Exp* end;
+    Block* block;
+    ForStatement(){};
+    int accept(Visitor* visitor);
+    ~ForStatement();
+};
+
+class WhileStatement : public Stm {
+   public:
+    Exp* condition;
+    Block* block;
+    WhileStatement(Exp* c, Block* b);
+    ~WhileStatement();
+    int accept(Visitor* visitor);
 };
 
 class IfStatement : public Stm {
- public:
-  Exp* condition;
-  Body* then;
-  Body* els;
-  IfStatement(Exp* condition, Body* then, Body* els);
-  int accept(Visitor* visitor);
-  ~IfStatement();
+   public:
+    Exp* condition;
+    Block* then;
+    Block* els;
+    IfStatement(Exp* condition, Block* then, Block* els);
+    IfStatement(){};
+    int accept(Visitor* visitor);
+    ~IfStatement();
+};
+
+class Block {
+   public:
+    VarDecList* vardecl;
+    StmtList* stmdecl;
+    Block(){};
+    ~Block();
+    int accept(Visitor* visitor);
+};
+
+class FunDec {
+   public:
+    string id;
+    string type;
+    list<string> param_list;
+    list<string> tipos;
+    Block* block;
+    FunDec(){};
+    ~FunDec();
+    int accept(Visitor* visitor);
+};
+
+class FunDecList {
+   public:
+    list<FunDec*> fundecs;
+    void add(FunDec* fundec) { fundecs.push_back(fundec); };
+    FunDecList(){};
+    ~FunDecList();
+    int accept(Visitor* visitor);
 };
 
 class VarDec {
- public:
-  string type;
-  list<string> vars;
-  VarDec(string type, list<string> vars);
-  int accept(Visitor* visitor);
-  ~VarDec();
+   public:
+    bool is_mut;
+    string type;
+    string var;
+    Exp* exp;
+    VarDec(){};
+    int accept(Visitor* visitor);
+    ~VarDec();
 };
 
 class VarDecList {
- public:
-  list<VarDec*> vardecs;
-  VarDecList();
-  void add(VarDec* vardec);
-  int accept(Visitor* visitor);
-  ~VarDecList();
+   public:
+    list<VarDec*> vardecs;
+    void add(VarDec* vardec);
+    VarDecList(){};
+    ~VarDecList();
+    int accept(Visitor* visitor);
 };
 
-class StatementList {
- public:
-  list<Stm*> stms;
-  StatementList();
-  void add(Stm* stm);
-  int accept(Visitor* visitor);
-  ~StatementList();
+class ClassDec {
+   public:
+    string id;
+    list<VarDec*> vardecs;
+    ClassDec(){};
+    ~ClassDec();
+    int accept(Visitor* visitor);
 };
 
-class Body {
- public:
-  VarDecList* vardecs;
-  StatementList* slist;
-  Body(VarDecList* vardecs, StatementList* stms);
-  int accept(Visitor* visitor);
-  ~Body();
+class ClassDecList {
+   public:
+    list<ClassDec*> classdecs;
+    ClassDecList(){};
+    ~ClassDecList();
+    int accept(Visitor* visitor);
 };
 
 class Program {
- public:
-  Body* body;
-  Program(Body* body);
-  int contar() {
-    int a = 0;
-    for (auto i : body->vardecs->vardecs) {
-      a += i->vars.size();
-    }
-    return a;
-  }
-  ~Program();
+   public:
+    VarDecList* vl;
+    FunDecList* fl;
+    ClassDecList* cl;
+    StmtList* stml;
+    Program() : vl(nullptr), fl(nullptr), cl(nullptr), stml(nullptr) {}
+    ~Program();
 };
 
 #endif
