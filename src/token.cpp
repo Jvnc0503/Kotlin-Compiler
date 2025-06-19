7a8a83125b1a124f4359bb5e853e7440d04b88d1
m41k1 <michalhinojosa@gmail.com>
2025-06-18 23:50:58 -0500

    feat: tokencpp y token.h

--- src/token.cpp
+++ src/token.cpp
@@ -3,70 +3,71 @@
 #include <ostream>
 #include <unordered_map>
 
-Token::Token(const Type type, const int line, const int column)
-    : type(type), line(line), column(column) {}
+Token::Token(const Type type, const int line, const int column) : type(type), line(line), column(column) {}
 
-Token::Token(const Type type, const char c, const int line, const int column)
-    : type(type), text(std::string(1, c)), line(line), column(column) {}
+Token::Token(const Type type, const char c, const int line, const int column) : type(type), text(std::string(1, c)), line(line), column(column) {}
 
-Token::Token(const Type type, const std::string &source, const int &first, const int &length,
-             const int line, const int column)
+Token::Token(const Type type, const std::string& source, const int& first, const int& length, const int line, const int column)
     : type(type), text(source.substr(first, length)), line(line), column(column) {}
 
-std::string Token::typeToString(const Type &type) {
-  static const std::unordered_map<Type, std::string> types = {{PLUS, "PLUS"},
-                                                              {MINUS, "MINUS"},
-                                                              {MUL, "MUL"},
-                                                              {DIV, "DIV"},
-                                                              {LT, "LT"},
-                                                              {GT, "GT"},
-                                                              {LTE, "LTE"},
-                                                              {GTE, "GTE"},
-                                                              {EQEQ, "EQEQ"},
-                                                              {NEQ, "NEQ"},
-                                                              {AND, "AND"},
-                                                              {OR, "OR"},
-                                                              {NOT, "NOT"},
-                                                              {ASSIGN, "ASSIGN"},
-                                                              {COLON, "COLON"},
-                                                              {SEMICOLON, "SEMICOLON"},
-                                                              {DOT, "DOT"},
-                                                              {COMMA, "COMMA"},
-                                                              {RANGE, "RANGE"},
-                                                              {ENDL, "ENDL"},
-                                                              {LPAREN, "LPAREN"},
-                                                              {RPAREN, "RPAREN"},
-                                                              {LBRACE, "LBRACE"},
-                                                              {RBRACE, "RBRACE"},
-                                                              {IF, "IF"},
-                                                              {ELSE, "ELSE"},
-                                                              {WHILE, "WHILE"},
-                                                              {FOR, "FOR"},
-                                                              {BREAK, "BREAK"},
-                                                              {CONTINUE, "CONTINUE"},
-                                                              {RETURN, "RETURN"},
-                                                              {IN, "IN"},
-                                                              {VAR, "VAR"},
-                                                              {VAL, "VAL"},
-                                                              {FUN, "FUN"},
-                                                              {CLASS, "CLASS"},
-                                                              {THIS, "THIS"},
-                                                              {INT_TYPE, "INT_TYPE"},
-                                                              {BOOL_TYPE, "BOOL_TYPE"},
-                                                              {UNIT_TYPE, "UNIT_TYPE"},
-                                                              {INT_LITERAL, "INT_LITERAL"},
-                                                              {TRUE, "TRUE"},
-                                                              {FALSE, "FALSE"},
-                                                              {ID, "ID"},
-                                                              {ENDOFFILE, "ENDOFFILE"},
-                                                              {ERROR, "ERROR"}};
-  return types.contains(type) ? types.at(type) : "UNKNOWN";
+std::string Token::typeToString(const Type& type) {
+    static const std::unordered_map<Type, std::string> types = {{PLUS, "PLUS"},
+                                                                {MINUS, "MINUS"},
+                                                                {MUL, "MUL"},
+                                                                {DIV, "DIV"},
+                                                                {LT, "LT"},
+                                                                {GT, "GT"},
+                                                                {LTE, "LTE"},
+                                                                {GTE, "GTE"},
+                                                                {EQEQ, "EQEQ"},
+                                                                {NEQ, "NEQ"},
+                                                                {AND, "AND"},
+                                                                {OR, "OR"},
+                                                                {NOT, "NOT"},
+                                                                {ASSIGN, "ASSIGN"},
+                                                                {COLON, "COLON"},
+                                                                {SEMICOLON, "SEMICOLON"},
+                                                                {DOT, "DOT"},
+                                                                {COMMA, "COMMA"},
+                                                                {RANGE, "RANGE"},
+                                                                {ENDL, "ENDL"},
+                                                                {LPAREN, "LPAREN"},
+                                                                {RPAREN, "RPAREN"},
+                                                                {LBRACE, "LBRACE"},
+                                                                {RBRACE, "RBRACE"},
+                                                                {IF, "IF"},
+                                                                {ELSE, "ELSE"},
+                                                                {WHILE, "WHILE"},
+                                                                {FOR, "FOR"},
+                                                                {BREAK, "BREAK"},
+                                                                {CONTINUE, "CONTINUE"},
+                                                                {RETURN, "RETURN"},
+                                                                {IN, "IN"},
+                                                                {VAR, "VAR"},
+                                                                {VAL, "VAL"},
+                                                                {FUN, "FUN"},
+                                                                {CLASS, "CLASS"},
+                                                                {THIS, "THIS"},
+                                                                {INT_TYPE, "INT_TYPE"},
+                                                                {BOOL_TYPE, "BOOL_TYPE"},
+                                                                {UNIT_TYPE, "UNIT_TYPE"},
+                                                                {INT_LITERAL, "INT_LITERAL"},
+                                                                {TRUE, "TRUE"},
+                                                                {FALSE, "FALSE"},
+                                                                {ID, "ID"},
+                                                                {ENDOFFILE, "ENDOFFILE"},
+                                                                {PRINT, "PRINT"},
+                                                                {PRINTLN, "PRINTLN"},
+                                                                {ERROR, "ERROR"}};
+    return types.contains(type) ? types.at(type) : "UNKNOWN";
 }
 
-std::ostream &operator<<(std::ostream &os, const Token &t) {
-  os << "TOKEN(" << Token::typeToString(t.type) << ", " << t.text << ")";
-  os << "\tline " << t.line << ", column " << t.column << '\n';
-  return os;
+std::ostream& operator<<(std::ostream& os, const Token& t) {
+    os << "TOKEN(" << Token::typeToString(t.type) << ", " << t.text << ")";
+    os << "\tline " << t.line << ", column " << t.column << '\n';
+    return os;
 }
 
-std::ostream &operator<<(std::ostream &os, const Token *t) { return os << *t; }
+std::ostream& operator<<(std::ostream& os, const Token* t) {
+    return os << *t;
+}
--- src/token.h
+++ src/token.h
@@ -4,95 +4,96 @@
 #include <string>
 
 struct Token {
-  enum Type {
-    // Arithmetic operators
-    PLUS,
-    MINUS,
-    MUL,
-    DIV,
+    enum Type {
+        // Arithmetic operators
+        PLUS,
+        MINUS,
+        MUL,
+        DIV,
 
-    // Comparison operators
-    LT,
-    GT,
-    LTE,
-    GTE,
-    EQEQ,
-    NEQ,
+        // Comparison operators
+        LT,
+        GT,
+        LTE,
+        GTE,
+        EQEQ,
+        NEQ,
 
-    // Bolean operators
-    AND,
-    OR,
-    NOT,
+        // Bolean operators
+        AND,
+        OR,
+        NOT,
 
-    // Delimiters
-    ASSIGN,
-    COLON,
-    SEMICOLON,
-    DOT,
-    COMMA,
-    RANGE,
-    ENDL,
+        // Delimiters
+        ASSIGN,
+        COLON,
+        SEMICOLON,
+        DOT,
+        COMMA,
+        RANGE,
+        ENDL,
 
-    // Grouping
-    LPAREN,
-    RPAREN,
-    LBRACE,
-    RBRACE,
+        // Grouping
+        LPAREN,
+        RPAREN,
+        LBRACE,
+        RBRACE,
 
-    // Control Structures
-    IF,
-    ELSE,
-    WHILE,
-    FOR,
-    BREAK,
-    CONTINUE,
-    RETURN,
-    IN,
+        // Control Structures
+        IF,
+        ELSE,
+        WHILE,
+        FOR,
+        BREAK,
+        CONTINUE,
+        RETURN,
+        IN,
 
-    // Declarations
-    VAR,
-    VAL,
-    FUN,
-    CLASS,
+        // Declarations
+        VAR,
+        VAL,
+        FUN,
+        CLASS,
 
-    // References
-    THIS,
+        // References
+        THIS,
 
-    // Types
-    INT_TYPE,
-    BOOL_TYPE,
-    UNIT_TYPE,
+        // Types
+        INT_TYPE,
+        BOOL_TYPE,
+        UNIT_TYPE,
 
-    // Values
-    INT_LITERAL,
-    TRUE,
-    FALSE,
-    ID,
+        // Values
+        INT_LITERAL,
+        TRUE,
+        FALSE,
+        ID,
 
-    // Extra
-    ENDOFFILE,
-    ERROR
-  };
+        // Extra
+        ENDOFFILE,
+        ERROR,
+        PRINT,
+        PRINTLN
+    };
 
-  Type type;
-  std::string text;
-  int line;
-  int column;
+    Type type;
+    std::string text;
+    int line;
+    int column;
 
-  Token(Type type, int line, int column);
+    Token(Type type, int line, int column);
 
-  Token(Type type, char c, int line, int column);
+    Token(Type type, char c, int line, int column);
 
-  Token(Type type, const std::string &source, const int &first, const int &length, int line,
-        int column);
+    Token(Type type, const std::string& source, const int& first, const int& length, int line, int column);
 
-  ~Token() = default;
+    ~Token() = default;
 
-  static std::string typeToString(const Type &type);
+    static std::string typeToString(const Type& type);
 
-  friend std::ostream &operator<<(std::ostream &os, const Token &t);
+    friend std::ostream& operator<<(std::ostream& os, const Token& t);
 
-  friend std::ostream &operator<<(std::ostream &os, const Token *t);
+    friend std::ostream& operator<<(std::ostream& os, const Token* t);
 };
 
 #endif  // TOKEN_H
