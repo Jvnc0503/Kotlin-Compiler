214793e83c89e459ca9fdd0e1f13bd54e83ad49a
m41k1 <michalhinojosa@gmail.com>
2025-06-20 13:14:35 -0500

    reorganizacion de archivos para implementar al api de aws lambda y el frontend en react

---
+++ compiler/ejecutables/output.s
@@ -0,0 +1,20 @@
+.data
+print_fmt:     .string "%ld "
+print_fmt_ln:  .string "%ld \n"
+.text
+.globl main
+main:
+ pushq %rbp
+ movq %rsp, %rbp
+ movq $1, %rax
+ movq %rax, -8(%rbp)
+ subq $16, %rsp
+ movq -8(%rbp), %rax
+ movq %rax, %rsi
+ leaq print_fmt(%rip), %rdi
+ movl $0, %eax
+ call printf@PLT
+.end_main:
+leave
+ret
+.section .note.GNU-stack,"",@progbits
