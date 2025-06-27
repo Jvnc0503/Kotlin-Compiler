a803eb73dff260e470258f6d5d76475e986c7914
m41k1 <michalhinojosa@gmail.com>
2025-06-27 01:14:03 -0500

    feat: ejecutable que corre todos los inputs

--- compiler/ejecutables/output.s
+++ compiler/ejecutables/output.s
@@ -2,46 +2,44 @@
 print_fmt:     .string "%ld "
 print_fmt_ln:  .string "%ld \n"
 .text
-.globl sumar
-sumar:
- pushq %rbp
- movq %rsp, %rbp
- movq %rdi,-8(%rbp)
- movq %rsi,-16(%rbp)
- subq $32, %rsp
- movq -8(%rbp), %rax
- pushq %rax
- movq -16(%rbp), %rax
- movq %rax, %rcx
- popq %rax
- addq %rcx, %rax
- jmp .end_sumar
-.end_sumar:
-leave
-ret
 .globl main
 main:
  pushq %rbp
  movq %rsp, %rbp
- movq $3, %rax
+ subq $8, %rsp
+ movq $0, %rax
  movq %rax, -8(%rbp)
- movq %rax, -8(%rbp)
- movq $5, %rax
- movq %rax, -16(%rbp)
- movq %rax, -16(%rbp)
+ subq $16, %rsp
+while_0:
  movq -8(%rbp), %rax
- mov %rax, %rdi
- movq -16(%rbp), %rax
- mov %rax, %rsi
-call sumar
- movq %rax, -24(%rbp)
- movq %rax, -24(%rbp)
- subq $32, %rsp
- movq -24(%rbp), %rax
+ pushq %rax
+ movq $5, %rax
+ movq %rax, %rcx
+ popq %rax
+ cmpq %rcx, %rax
+ movl $0, %eax
+ setle %al
+ movzbq %al, %rax
+ cmpq $0, %rax
+ je endwhile_0
+ movq -8(%rbp), %rax
+ movq %rsp, %rbx
+ andq $15, %rbx
+ subq %rbx, %rsp
  movq %rax, %rsi
  leaq print_fmt_ln(%rip), %rdi
- movl $0, %eax
+ xor  %eax, %eax
  call printf@PLT
+ addq %rbx, %rsp
+ movq -8(%rbp), %rax
+ pushq %rax
+ movq $1, %rax
+ movq %rax, %rcx
+ popq %rax
+ addq %rcx, %rax
+ movq %rax, -8(%rbp)
+ jmp while_0
+endwhile_0:
 .end_main:
 leave
 ret
